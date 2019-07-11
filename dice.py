2705d2a488bfcf161fac69984daabebdde855621
Takuma Furudate <s1250100@u-aizu.ac.jp>
2019-07-11 21:41:04 +0900

    increase dice

--- dice.py
+++ dice.py
@@ -6,13 +6,15 @@ print('Hello, ' +name+ '!')
 
 diceA = random.randint(1,6)
 diceB = random.randint(1,6)
+diceC = random.randint(1,6)
 print('Rolling the dice...')
 print('Die 1: '+str(diceA))
 print('Die 2: '+str(diceB))
+print('Die 2: '+str(diceC))
 
-total = diceA + diceB
+total = diceA + diceB + diceC
 print('Total value: '+str(total))
 
-if total > 7:
+if total > 10:
     print(+name+ ' won')
 else: print(+name+ ' lost')
