7ac242165d842546d9f31f85fc1fec576e206c08
Takuma Furudate <s1250100@u-aizu.ac.jp>
2019-07-11 21:22:53 +0900

    Initial revision

---
+++ dice.py
@@ -0,0 +1,10 @@
+import random
+
+diceA = random.randint(1,6)
+diceB = random.randint(1,6)
+print('Rolling the dice...')
+print('Die 1: '+str(diceA))
+print('Die 2: '+str(diceB))
+
+total = diceA + diceB
+print('Total value: '+str(total))
